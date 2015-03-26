d027e364cc0f7261966411bdfb300877fa614c10
Wilfredo Sanchez <wsanchez@apple.com>
2015-03-26 22:59:22 +0000

    Spiff up
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/PyKerberos/trunk@14626 e27351fd-9f3e-4f54-a53b-843176b1656c

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@
 # limitations under the License.
 ##
 
-from distutils.core import setup, Extension
+from os.path import dirname, join as joinpath
+from setuptools import setup, Extension
 
 try:
     from subprocess import getoutput
@@ -22,42 +23,110 @@ except ImportError:
     from commands import getoutput
 
 
-long_description = """
-This Python package is a high-level wrapper for Kerberos (GSSAPI) operations.
-The goal is to avoid having to build a module that wraps the entire
-Kerberos.framework, and instead offer a limited set of functions that do what
-is needed for client/server Kerberos authentication based on
-<http://www.ietf.org/rfc/rfc4559.txt>.
-"""
+#
+# Options
+#
+
+project_name = "kerberos"
+
+version_string = "1.2.0"
+
+description = "Kerberos high-level interface"
+
+long_description = file(joinpath(dirname(__file__), "README.rst")).read()
+
+url = "http://www.calendarserver.org/"
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: System :: Systems Administration :: Authentication/Directory",
+]
+
+author = "Apple Inc."
+
+author_email = "calendarserver-dev@lists.macosforge.org"
+
+license = "Apache License, Version 2.0"
+
+platforms = ["all"]
+
+
+#
+# Entry points
+#
+
+entry_points = {
+    "console_scripts": [],
+}
+
+
+#
+# Dependencies
+#
+
+setup_requirements = []
+
+install_requirements = []
+
+extras_requirements = {}
 
 extra_link_args = getoutput("krb5-config --libs gssapi").split()
+
 extra_compile_args = getoutput("krb5-config --cflags gssapi").split()
 
 
-setup(
-    name="kerberos",
-    version="1.2.1",
-    description="Kerberos high-level interface",
-    long_description=long_description,
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Systems Administration :: Authentication/Directory"
-    ],
-    ext_modules=[
-        Extension(
-            "kerberos",
-            extra_link_args=extra_link_args,
-            extra_compile_args=extra_compile_args,
-            sources=[
-                "src/base64.c",
-                "src/kerberos.c",
-                "src/kerberosbasic.c",
-                "src/kerberosgss.c",
-                "src/kerberospw.c",
-            ],
-        ),
-    ],
-)
+#
+# Set up Extension modules that need to be built
+#
+
+extensions = [
+    Extension(
+        "kerberos",
+        extra_link_args=extra_link_args,
+        extra_compile_args=extra_compile_args,
+        sources=[
+            "src/base64.c",
+            "src/kerberos.c",
+            "src/kerberosbasic.c",
+            "src/kerberosgss.c",
+            "src/kerberospw.c",
+        ],
+    ),
+]
+
+
+#
+# Run setup
+#
+
+def doSetup():
+    setup(
+        name=project_name,
+        version=version_string,
+        description=description,
+        long_description=long_description,
+        url=url,
+        classifiers=classifiers,
+        author=author,
+        author_email=author_email,
+        license=license,
+        platforms=platforms,
+        ext_modules=extensions,
+        setup_requires=setup_requirements,
+        install_requires=install_requirements,
+        extras_require=extras_requirements,
+    )
+
+
+#
+# Main
+#
+
+if __name__ == "__main__":
+    doSetup()
